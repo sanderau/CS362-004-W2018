3898c4f1795ba53568cb9f890b16e0978149f687
Austin Sanders <sanderau@oregonstate.edu>
2018-02-04 22:03:33 -0800

    done with unit test 3 and updating 2

--- dominion/unittest3.c
+++ dominion/unittest3.c
@@ -5,8 +5,55 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define NOISY 1 // set to 0 if you dont want all the printfs
+
 int main(void)
 {
+	int cardPos = 5;
+	int players = 0;
+	int k[10] = {adventurer, council_room, feast, gardens, mine,
+	       remodel, smithy, village, baron, great_hall};
+	int seed = 1000;
+	struct gameState G;
+
+	initializeGame(players, k, seed, &G);
+
+	//numbuys set to 0;
+	G.numBuys = 0;
+
+	if(buyCard(cardPos, &G) == -1 && NOISY)
+	{
+		printf("No buys test passed\n");
+	}
+
+	else if(NOISY)
+	{
+		printf("No buys test failed\n");
+		exit(1);
+	}
+
+	//none of that card
+
+	G.supplyCount[cardPos] = 0;
+
+	if(buyCard(cardPos, &G) == -1 && NOISY)
+	{
+		printf("No card test passed\n");
+	}
+
+	else if(NOISY)
+	{
+		printf("No card test failed\n");
+		exit(1);
+	}
+
+
+	//done
+
+	printf("all tests passed\n");
+
 
 	return 0;
+
 }
+
